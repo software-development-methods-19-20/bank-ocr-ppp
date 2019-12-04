d48a844c08b879e6e0db84860c7531f2cc64d5cb
MarcoAlbertoGrimaldi <marco.alberto.grimaldi@gmail.com>
2019-12-04 10:15:55 +0100

    test

--- src/main/java/bankocr/kata/Cell.java
+++ src/main/java/bankocr/kata/Cell.java
@@ -1,33 +1,61 @@
 package bankocr.kata;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Cell {
 
-    private static final String ZERO_CELL =
+    private static final String ZERO =
                     " _ " +
                     "| |" +
                     "|_|";
 
-    private static final String NINE_CELL =
+    private static final String NINE =
                     " _ " +
                     "|_|" +
                     " _|";
 
-    private static final String EIGHT_CELL =
+    private static final String EIGHT =
                     " _ " +
                     "|_|" +
                     "|_|";
 
-    private static final String SEVEN_CELL =
+    private static final String SEVEN =
                     " _ " +
                     "  |" +
                     "  |";
 
-    private static final String SIX_CELL =
+    private static final String SIX =
                     " _ " +
                     "|_ " +
                     "|_|";
 
-    private static final String[] NUM_CELL = new String[10]{ZERO_CELL};
+    private static final String FIVE =
+                    " _ " +
+                    "|_ " +
+                    " _|";
+
+    private static final String FOUR =
+                    "   " +
+                    "|_| " +
+                    "  |";
+
+    private static final String THREE =
+                    " _ " +
+                    " _| " +
+                    " _|";
+
+    private static final String TWO =
+                    " _ " +
+                    " _|" +
+                    "|_ ";
+
+    private static final String ONE =
+                    "   " +
+                    "  |" +
+                    "  |";
+
+    private static final String[] NUM_CELL = {ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE};
 
     private String cellAsText;
 
@@ -37,6 +65,10 @@ public class Cell {
 
     @Override
     public String toString() {
-        return ZERO_CELL.equals(cellAsText) ? "0": "1";
+        for(int i = 0; i<10; ++i){
+            if(NUM_CELL[i].equals(cellAsText)) return Integer.toString(i);
+        }
+        return null;
     }
+
 }
